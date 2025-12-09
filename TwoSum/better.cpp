c91815dc64a542ce4a4e5dd4fd6e31c79d5cc950
anirudhkumaratrey <anirudhatrey@gmail.com>
2025-12-09 16:48:42 +0530

    Two sum Brute force

---
+++ TwoSum/better.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int,int> twosum(int arr[], int n, int k){
+    for(int i = 0; i < n ; i ++){
+        for(int j = i; j < n ; j ++){
+            if(arr[i]+arr[j] == k){
+                return   make_pair(i+1,j+1);
+            }
+        }
+    }
+return make_pair(-1,-1);
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+
+    int n, ans = 0, k;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> k;
+    int sum = 0;
+   pair<int,int> a = twosum(arr,n,k);
+   cout<< a.first<<", "<<a.second;
+    return 0
+}
