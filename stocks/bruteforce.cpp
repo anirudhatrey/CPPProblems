aaddba03d65cf8745a9f26a3c9a3906fde721442
anirudhatrey <anirudhatrey@gmail.com>
2026-01-04 18:27:00 +0530

    stock max profit brute force

---
+++ stocks/bruteforce.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int stocks(int arr[], int n, int k)
+{
+    int zeroCount = 0, oneCount = 0, twoCount = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (zeroCount < arr[j] - arr[i])
+            {
+                zeroCount = arr[j] - arr[i];
+            }
+        }
+    }
+
+    
+   cout<<zeroCount;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+
+    int n, ans = 0, k;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> k;
+    int sum = 0;
+    stocks(arr, n, k);
+    return 0;
+}
---
+++ stocks/optimal.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sort012(int arr[], int n, int k)
+{
+    int zeroCount = 0, oneCount = 0, twoCount = 0;
+    for (int i = 0; i < n;i++)
+    {
+        if (arr[i] == 0)
+        {
+            zeroCount++;
+        }
+        else if (arr[i] == 1)
+        {
+            oneCount++;
+        }
+        else if (arr[i] == 2)
+        {
+            twoCount++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i]=i<zeroCount?0:i<zeroCount+oneCount?1:2;
+    }
+
+    for (int i = 0; i < n;i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+
+    int n, ans = 0, k;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> k;
+    int sum = 0;
+    sort012(arr, n, k);
+    return 0;
+}
